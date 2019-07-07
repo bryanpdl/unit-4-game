f0412fe33bd4b589969a67af3e84c20a77bc6003
bryanpdl <bryan@ui8.net>
2019-07-07 10:36:08 -0700

    Final Game

--- game.js
+++ game.js
@@ -1,8 +1,125 @@
 
+// Variables for different value possibilites
+
+
+var givenArr = [36, 28, 41, 33, 22, 27, 38, 65, 52, 19];
+
+var circArr = [ 7, 2, 11, 17, 9, 6, 3];
+
+var sqrArr = [ 3, 16, 8, 13, 1, 7, 2, 9];
+
+var dmdArr = [ 5, 12, 10, 3, 7, 1, 6, 17,];
+
+var hrglassArr = [ 4, 15, 11, 9, 1, 16, 8, 3];
+
+var circNum;
+
+var sqrNum;
+
+var dmdNum;
+
+var hrglassNum;
+
+var givenNum;
+
+var score = 0;
+
+var wins = 0;
+
+var losses = 0;
 
 
 
-$("#element td").css('border-color', 'none');
+
+function init() {
+
+    
+
+givenNum = givenArr[Math.floor(Math.random() * givenArr.length)];
+console.log(givenNum);
+
+$('#givenNum').text(givenNum);
+score = 0;
+circNum = circArr[Math.floor(Math.random() * circArr.length)];
+sqrNum = sqrArr[Math.floor(Math.random() * sqrArr.length)];
+dmdNum = sqrArr[Math.floor(Math.random() * dmdArr.length)];
+hrglassNum = sqrArr[Math.floor(Math.random() * hrglassArr.length)];
 
 
-$("#element td:nth-child(2, 3, 4, 5, 6)");
+    
+}
+    $("#circBadge").on('click', function() {
+        
+        console.log(circNum);
+    
+        updateScore(circNum);
+        $('#userScore').fadeIn().text(score);
+        
+        
+    });
+    
+    $("#sqrBadge").on('click', function() {
+       
+        console.log(sqrNum);
+        updateScore(sqrNum);
+        $('#userScore').text(score);
+    
+    
+    });
+    
+    $("#diamondBadge").on('click', function() {
+        
+        console.log(dmdNum);
+        updateScore(dmdNum);
+        $('#userScore').text(score);
+    
+    
+    });
+    
+    $("#hourglassBadge").on('click', function() {
+        
+        console.log(hrglassNum);
+        updateScore(hrglassNum);
+        $('#userScore').text(score);
+    
+        
+    });
+    
+    function updateScore(value) {
+    score+= value
+    
+    if (score > givenNum) {
+       console.log("loss");
+       losses++
+       $('#lossNum').text("Losses: " + losses);
+       init();
+      
+       
+       
+       
+    } else if (score === givenNum) {
+        console.log("win");
+        wins++
+        $('#winNum').text("Wins: " + wins);
+        init();
+        
+    } 
+    
+}
+
+init();
+
+    
+
+
+
+
+//function updateScore () {
+
+   // var updatedScore = [currentScore += updatedScore]
+   // $('crystal-count').text('Current Score:  ' + updatedScore)
+//};
+
+
+
+
